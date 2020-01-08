b16c30e843051f5d4f7330179a9ceb61576cbbdf
phtseng <phtseng@nuvoton.com>
2020-01-08 13:47:50 +0800

    Fixed warnings of adc driver
    Change-Id: Ic1194ac5cf63015154af0c597a43eaa8c491f027

--- Library/StdDriver/src/adc.c
+++ Library/StdDriver/src/adc.c
@@ -45,11 +45,11 @@ void ADC_Open(ADC_T *adc,
               uint32_t u32ChMask)
 {
 
-    ADC->ADCR = (ADC->ADCR & (~(ADC_ADCR_DIFFEN_Msk | ADC_ADCR_ADMD_Msk))) | \
-                u32InputMode | \
-                u32OpMode;
+    (adc)->ADCR = ((adc)->ADCR & (~(ADC_ADCR_DIFFEN_Msk | ADC_ADCR_ADMD_Msk))) | \
+                (u32InputMode) | \
+                (u32OpMode);
 
-    ADC->ADCHER = (ADC->ADCHER & ~ADC_ADCHER_CHEN_Msk) | (u32ChMask);
+    (adc)->ADCHER = ((adc)->ADCHER & ~ADC_ADCHER_CHEN_Msk) | (u32ChMask);
 
     return;
 }
@@ -61,6 +61,7 @@ void ADC_Open(ADC_T *adc,
   */
 void ADC_Close(ADC_T *adc)
 {
+    (void) adc;
     SYS->IPRSTC2 |= SYS_IPRSTC2_ADC_RST_Msk;
     SYS->IPRSTC2 &= ~SYS_IPRSTC2_ADC_RST_Msk;
     return;
@@ -88,15 +89,15 @@ void ADC_EnableHWTrigger(ADC_T *adc,
                          uint32_t u32Source,
                          uint32_t u32Param)
 {
-    ADC->ADCR &= ~(ADC_ADCR_TRGS_Msk | ADC_ADCR_TRGCOND_Msk | ADC_ADCR_TRGEN_Msk);
+    (adc)->ADCR &= ~(ADC_ADCR_TRGS_Msk | ADC_ADCR_TRGCOND_Msk | ADC_ADCR_TRGEN_Msk);
     if(u32Source == ADC_ADCR_TRGS_STADC)
     {
-        ADC->ADCR |= u32Source | u32Param | ADC_ADCR_TRGEN_Msk;
+        (adc)->ADCR |= (u32Source) | (u32Param) | ADC_ADCR_TRGEN_Msk;
     }
     else
     {
-        ADC->ADTDCR = (ADC->ADTDCR & ~ADC_ADTDCR_PTDT_Msk) | u32Param;
-        ADC->ADCR |= u32Source | ADC_ADCR_TRGEN_Msk;
+        (adc)->ADTDCR = ((adc)->ADTDCR & ~ADC_ADTDCR_PTDT_Msk) | (u32Param);
+        (adc)->ADCR |= (u32Source) | ADC_ADCR_TRGEN_Msk;
     }
     return;
 }
@@ -108,7 +109,7 @@ void ADC_EnableHWTrigger(ADC_T *adc,
   */
 void ADC_DisableHWTrigger(ADC_T *adc)
 {
-    ADC->ADCR &= ~(ADC_ADCR_TRGS_Msk | ADC_ADCR_TRGCOND_Msk | ADC_ADCR_TRGEN_Msk);
+    (adc)->ADCR &= ~(ADC_ADCR_TRGS_Msk | ADC_ADCR_TRGCOND_Msk | ADC_ADCR_TRGEN_Msk);
     return;
 }
 
@@ -125,12 +126,12 @@ void ADC_DisableHWTrigger(ADC_T *adc)
   */
 void ADC_EnableInt(ADC_T *adc, uint32_t u32Mask)
 {
-    if(u32Mask & ADC_ADF_INT)
-        ADC->ADCR |= ADC_ADCR_ADIE_Msk;
-    if(u32Mask & ADC_CMP0_INT)
-        ADC->ADCMPR[0] |= ADC_ADCMPR_CMPIE_Msk;
-    if(u32Mask & ADC_CMP1_INT)
-        ADC->ADCMPR[1] |= ADC_ADCMPR_CMPIE_Msk;
+    if((u32Mask) & ADC_ADF_INT)
+        (adc)->ADCR |= ADC_ADCR_ADIE_Msk;
+    if((u32Mask) & ADC_CMP0_INT)
+        (adc)->ADCMPR[0] |= ADC_ADCMPR_CMPIE_Msk;
+    if((u32Mask) & ADC_CMP1_INT)
+        (adc)->ADCMPR[1] |= ADC_ADCMPR_CMPIE_Msk;
 
     return;
 }
@@ -148,12 +149,12 @@ void ADC_EnableInt(ADC_T *adc, uint32_t u32Mask)
   */
 void ADC_DisableInt(ADC_T *adc, uint32_t u32Mask)
 {
-    if(u32Mask & ADC_ADF_INT)
-        ADC->ADCR &= ~ADC_ADCR_ADIE_Msk;
-    if(u32Mask & ADC_CMP0_INT)
-        ADC->ADCMPR[0] &= ~ADC_ADCMPR_CMPIE_Msk;
-    if(u32Mask & ADC_CMP1_INT)
-        ADC->ADCMPR[1] &= ~ADC_ADCMPR_CMPIE_Msk;
+    if((u32Mask) & ADC_ADF_INT)
+        (adc)->ADCR &= ~ADC_ADCR_ADIE_Msk;
+    if((u32Mask) & ADC_CMP0_INT)
+        (adc)->ADCMPR[0] &= ~ADC_ADCMPR_CMPIE_Msk;
+    if((u32Mask) & ADC_CMP1_INT)
+        (adc)->ADCMPR[1] &= ~ADC_ADCMPR_CMPIE_Msk;
 
     return;
 }
